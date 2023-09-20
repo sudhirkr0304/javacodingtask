b946059ceddd3ce53271a1f37e6032a77c1dbef5
sudhir kumar <sudhir.kumar@netcracker.com>
2023-09-20 23:02:03 +0530

    created user model with role

--- src/main/java/com/sudhirkumar/javacodingtask/model/User.java
+++ src/main/java/com/sudhirkumar/javacodingtask/model/User.java
@@ -1,19 +1,23 @@
 package com.sudhirkumar.javacodingtask.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
 
     private String username;
 
@@ -21,17 +25,20 @@ public class User {
 
     private String email;
 
+    @Enumerated(EnumType.STRING)
     private Role role;
 
-    public User(Long id, String username, String password, String email, Role role) {
+    public User(Long id, String name, String username, String password, String email, Role role) {
         this.id = id;
+        this.name = name;
         this.username = username;
         this.password = password;
         this.email = email;
         this.role = role;
     }
 
-    public User(String username, String password, String email, Role role) {
+    public User(String name, String username, String password, String email, Role role) {
+        this.name = name;
         this.username = username;
         this.password = password;
         this.email = email;
@@ -41,6 +48,8 @@ public class User {
     public User() {
     }
 
+
+
     public Long getId() {
         return id;
     }
@@ -49,14 +58,48 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
