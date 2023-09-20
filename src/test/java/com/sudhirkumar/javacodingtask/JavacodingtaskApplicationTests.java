c911c9cdc4a854b4617aa280d7f940417ba67c7b
sudhir kumar <sudhir.kumar@netcracker.com>
2023-09-20 19:56:58 +0530

    Initial commit for java coding task

---
+++ src/test/java/com/sudhirkumar/javacodingtask/JavacodingtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhirkumar.javacodingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodingtaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
