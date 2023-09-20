b96902f08d67dbae10cdce69a67bcf58306d6add
sudhir kumar <sudhir.kumar@netcracker.com>
2023-09-20 23:36:27 +0530

    added token generation and token validation in jwt service

--- src/main/java/com/sudhirkumar/javacodingtask/config/JwtService.java
+++ src/main/java/com/sudhirkumar/javacodingtask/config/JwtService.java
@@ -1,12 +1,18 @@
 package com.sudhirkumar.javacodingtask.config;
 
+import com.sudhirkumar.javacodingtask.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -30,5 +36,36 @@ public class JwtService {
 
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails user) {
+        return generateToken(new HashMap<>(),user);
+    }
+
+
+    public boolean isTokenValid(String token, UserDetails user) {
+        final String username = extractUsername(token);
+        return (username.equals(user.getUsername())) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date(System.currentTimeMillis()));
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+
+    public String generateToken(Map<String,Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 }
